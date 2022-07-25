196eeecb31191f5ef9c5c61598dd1813efce5eba
unknown <omkarsh2872001@gmail.com>
2022-07-25 23:12:10 +0530

    storeorder Files by omkar 25 jul 11pm

---
+++ src/components/navbar.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          ExcerTracker
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Exercises List
+              </Link>
+            </li>
+{/* -----omkar add storageorderlist To Nav bar */}
+<li className="nav-item">
+              <Link className="nav-link" to="/storeordercreatelist">
+                StoreOrder List
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">
+                Create Exercise Log
+              </Link>
+            </li>
+            {/* added users list by Arun */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/userlist">
+                Users List
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/user">
+                Create User
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/productslist">
+                Products list
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/productcreate">
+                Add Product
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/storeordercreate">
+                Add StoreOrder
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
---
+++ src/components/products-list.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+//import { Link } from "react-router-dom";
+
+const ProductList = () => {
+  const [Data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/products/").then((response) => {
+      response.data.map((data) => {
+        console.log(data);
+        setData((oldArray) => [...oldArray, data]);
+        return 0;
+      });
+    });
+  }, []);
+
+  /* const deleteProduct = (id) => {
+    axios.delete("http://localhost:5000/products/" + id).then((response) => {
+      console.log(response.data);
+    });
+
+    setData(
+      Data.filter((el) => {
+        return el._id !== id;
+      })
+    );
+  }; */
+  return (
+    <div>
+      <h3>Product List</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>skuid</th>
+            <th>product</th>
+            <th>origin</th>
+            <th>price</th>
+
+            <th>datetime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Data.map((data, index) => (
+            <tr key={index}>
+              <td>{data.skuid}</td>
+              <td>{data.product}</td>
+              <td>{data.origin}</td>
+              <td>{data.price}</td>
+
+              <td>{data.datetime.substring(0, 10)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default ProductList;
